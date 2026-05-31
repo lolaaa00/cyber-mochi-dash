d5f94dd403847e4cc5f80a78ae69771ab1b1e36f
Lolaaa <ayoolarachi@gmail.com>
2026-05-31 13:23:36 +0100

    Fix index route to render the game instead of Agent Hub

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,30 +1,5 @@
-import { useRef } from "react";
-import HeroSection from "@/components/HeroSection";
-import ComparisonSection from "@/components/ComparisonSection";
-import FlowSection from "@/components/FlowSection";
-import ArchitectureSection from "@/components/ArchitectureSection";
-import CodePreview from "@/components/CodePreview";
-import FooterSection from "@/components/FooterSection";
+import EndlessRunner from "@/components/EndlessRunner";
 
-const Index = () => {
-  const comparisonRef = useRef<HTMLDivElement>(null);
-
-  const scrollToComparison = () => {
-    comparisonRef.current?.scrollIntoView({ behavior: "smooth" });
-  };
-
-  return (
-    <div className="min-h-screen bg-background">
-      <HeroSection onScrollToComparison={scrollToComparison} />
-      <div ref={comparisonRef}>
-        <ComparisonSection />
-      </div>
-      <FlowSection />
-      <ArchitectureSection />
-      <CodePreview />
-      <FooterSection />
-    </div>
-  );
-};
+const Index = () => <EndlessRunner />;
 
 export default Index;
